a3d4dea4d8c96243d9bb87394d77f2ac1efc993b
Cevher Dogan <cevherd@gmail.com>
2024-08-10 14:40:05 -0400

    Fixed topic_arn issues in index.html and routes.py. Confirmed subscription, posting worked thru webapp, view does not

--- awsops/sns/soft_switch/flask_app/app/routes.py
+++ awsops/sns/soft_switch/flask_app/app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, jsonify
-from .sns_utils import list_topics, publish_to_topic
+from .sns_utils import list_topics, publish_to_topic, subscribe_to_topic, unsubscribe_from_topic, list_subscriptions_by_topic
 
 main = Blueprint('main', __name__)
 
@@ -12,15 +12,49 @@ def index():
     topics = list_topics(profile_name, region_name)
 
     # Categorize topics
-    sms_topics = [topic['TopicArn'] for topic in topics if 'sms' in topic['TopicArn']]
-    email_topics = [topic['TopicArn'] for topic in topics if 'email' in topic['TopicArn']]
-    mixed_topics = [topic['TopicArn'] for topic in topics if 'mixed' in topic['TopicArn']]
+    sms_topics = [topic for topic in topics if 'sms' in topic.lower()]
+    email_topics = [topic for topic in topics if 'email' in topic.lower()]
+    mixed_topics = [topic for topic in topics if 'mixed' in topic.lower()]
+    uncategorized_topics = [topic for topic in topics if topic not in sms_topics + email_topics + mixed_topics]
 
     if request.method == 'POST':
-        topic_arn = request.form['topic_arn']
-        message = request.form['message']
-        response = publish_to_topic(topic_arn, message, profile_name, region_name)
-        return render_template('index.html', sms_topics=sms_topics, email_topics=email_topics, mixed_topics=mixed_topics, response=response)
+        action = request.form.get('action')
+        topic_arn = request.form.get('topic_arn')  # Retrieve topic_arn from the form
 
-    return render_template('index.html', sms_topics=sms_topics, email_topics=email_topics, mixed_topics=mixed_topics)
+        # Print or log the topic_arn for debugging
+        print(f"Debug: TopicArn received - {topic_arn}")
+
+        if not topic_arn:
+            return "Error: TopicArn is required", 400
+
+        if action == 'publish':
+            message = request.form['message']
+            response = publish_to_topic(topic_arn, message, profile_name, region_name)
+            return render_template('index.html', sms_topics=sms_topics, email_topics=email_topics, mixed_topics=mixed_topics, uncategorized_topics=uncategorized_topics, response=response)
+
+        elif action == 'subscribe':
+            protocol = request.form['protocol']
+            endpoint = request.form['endpoint']
+            response = subscribe_to_topic(topic_arn, protocol, endpoint, profile_name, region_name)
+            return render_template('index.html', sms_topics=sms_topics, email_topics=email_topics, mixed_topics=mixed_topics, uncategorized_topics=uncategorized_topics, response=response)
+
+        elif action == 'unsubscribe':
+            subscription_arn = request.form['subscription_arn']
+            response = unsubscribe_from_topic(subscription_arn, profile_name, region_name)
+            return render_template('index.html', sms_topics=sms_topics, email_topics=email_topics, mixed_topics=mixed_topics, uncategorized_topics=uncategorized_topics, response=response)
+
+    return render_template('index.html', sms_topics=sms_topics, email_topics=email_topics, mixed_topics=mixed_topics, uncategorized_topics=uncategorized_topics)
+
+@main.route('/subscriptions', methods=['GET'])
+def subscriptions():
+    profile_name = 'cevherdogan'
+    region_name = 'us-east-1'
+    topic_arn = request.args.get('topic_arn')
+    
+    if not topic_arn:
+        return "Error: TopicArn is required", 400
+    
+    subscriptions = list_subscriptions_by_topic(topic_arn, profile_name, region_name)
+    return render_template('subscriptions.html', subscriptions=subscriptions, topic_arn=topic_arn)
+
 
