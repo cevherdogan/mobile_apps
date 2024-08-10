eacbc87dcf06f76a9d363bf6b1f80b09e806a900
Cevher Dogan <cevherd@gmail.com>
2024-08-10 14:10:15 -0400

    Successfully tested basic UI operation to bring pull down based on topic type and publish messages to them

--- awsops/sns/soft_switch/flask_app/app/sns_utils.py
+++ awsops/sns/soft_switch/flask_app/app/sns_utils.py
@@ -1,22 +1,21 @@
 import boto3
 from botocore.exceptions import ClientError
 
+import boto3
+
 def list_topics(profile_name, region_name):
     session = boto3.Session(profile_name=profile_name)
     sns = session.client('sns', region_name=region_name)
-    return sns.list_topics()
+    return sns.list_topics()['Topics']
 
 def publish_to_topic(topic_arn, message, profile_name, region_name):
     session = boto3.Session(profile_name=profile_name)
     sns = session.client('sns', region_name=region_name)
-    try:
-        response = sns.publish(
-            TopicArn=topic_arn,
-            Message=message
-        )
-        return response
-    except ClientError as e:
-        return str(e)
+    response = sns.publish(
+        TopicArn=topic_arn,
+        Message=message
+    )
+    return response
 
 def subscribe_to_topic(topic_arn, protocol, endpoint, profile_name, region_name):
     session = boto3.Session(profile_name=profile_name)
