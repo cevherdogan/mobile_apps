73a669026985d958e0b361871e1d21db9ba0c3f9
Cevher Dogan <cevherd@gmail.com>
2024-08-11 00:59:59 -0400

    Something weird as it still does not open directl;y hwoever thsi link worked https://cevherdogan.github.io/mobile_apps/lambda_function_py.html

---
+++ copy_ccov.sh
@@ -0,0 +1,18 @@
+cp -r lambda_function/htmlcov/* .
+cp -r lambda_function/htmlcov/* coverage/
+
+
+git add .
+
+git add coverage/
+git commit -m "Add coverage report in coverage/ directory"
+git push origin gh-pages
+
+
+open https://cevherdogan.github.io/mobile_apps/coverage/
+open https://cevherdogan.github.io/mobile_apps
+
+
+https://cevherdogan.github.io/mobile_apps/lambda_function_py.html
+
+
